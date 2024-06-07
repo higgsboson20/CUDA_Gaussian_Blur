a42c32ef55b7f1f656f5ae72b0894dc2094f79ee
Ankit Goel <59352341+ankitgoel2001@users.noreply.github.com>
2024-06-07 01:11:14 -0700

    Add files via upload

--- gaussian_blur_serial.c
+++ gaussian_blur_serial.c
@@ -6,19 +6,6 @@
 
 #define PI 3.14159265358979323846
 
-/**This function is to create the gaussian kernel matrix 
-float** create_kernel_matrix(size_t kernel_order, uint32_t sigma) {
-    float kernel_matrix[kernel_order][kernel_order];
-    float normalizer = 1 / (2 * 3.14 * sigma * sigma);
-    for (size_t x = 0; x < kernel_order; x++) {
-        for (size_t y = 0; y < kernel_order; y++) {
-            kernel_matrix[x][y] = normalizer * exp(-1 * (x * x + y * y) / (2 * sigma * sigma));
-        }
-    }
-    return kernel_matrix;
-}
-**/
-
 int main(int argc, char * argv[])
 {   
     /**To ensure there are three arguments: <input_pgm>, <output_pgm> and <sigma>*/
@@ -29,43 +16,56 @@ int main(int argc, char * argv[])
     /**Parse the command line to recieve the input file, output file name and the sigma value*/
     const char* input_file = argv[1]; //input binary PGM file
     const char* output_file = argv[2]; //output binary PGM file
-    uint32_t sigma = atoi(argv[3]); //sigma value
+    float sigma = atof(argv[3]); //sigma value
 
-    char temp[200]; // temporary string to hold a copy of the input filename
-    uint32_t width = 0;
-
-    // Copy the filename to a temporary string
-    strcpy(temp, input_file);
-
-    // Find the position of the last underscore
-    char *underscore_pos = strrchr(temp, '_');
-    if (underscore_pos != NULL) {
-        // Find the position of the dot
-        char *dot_pos = strchr(underscore_pos, '.');
-        if (dot_pos != NULL) {
-            // Null-terminate the string at the dot position to isolate the number
-            *dot_pos = '\0';
-            // Convert the substring to an integer
-            width = atoi(underscore_pos + 1);
-        }
-    }
-    printf("%d\n", width);
-
-    /**Open the input binary PGM file*/
-    FILE* file = fopen(input_file, "rb");
-    if (file == NULL) {
-        perror("Cannot open file");
-        exit(1);
-    }
-
-    /**Validate the bounds of sigma*/
+    /**Validate the bounds of sigma and kernel*/
     if (sigma <= 0) {
         fprintf(stderr, "Sigma value must be greater than 0\n");
         return EXIT_FAILURE;
     }
-    
+
+    uint32_t width, height, max_value = 0;
+
+    /**Open the input binary PGM file*/
+    FILE* file = fopen(input_file, "rb");
+    if (!file) {
+        perror("Cannot open file");
+        exit(1);
+    }
+
+    // Read PGM header
+    fscanf(file, "P5\n%ld %ld\n%d\n", &width, &height, &max_value);
+    printf("%d\n", width);
+    printf("%d\n", height);
+    printf("%d\n", max_value);
+
+    //get the image matrix
+    float input_matrix[height][width];
+    for (size_t i = 0; i < height; i++) {
+            fread(input_matrix[i], sizeof(float), width, file);
+    }
+    fclose(file);
+
+/** 
+     for (size_t i = 0; i < height; i++) {
+        for (size_t j = 0; j < width; j++) {
+            printf("%.8f\t", input_matrix[i][j]);
+        }
+        printf("\n");
+    }
+    **/
+    printf("height: %d\n", height);
+    printf("width: %d\n", width);
+
     /**Create the gaussian kernel matrix*/
     uint32_t kernel_order = sigma * 6; //order of the kernel matrix
+    
+    /**Validate the bounds of the kernel matrix*/
+    if (kernel_order > width || kernel_order > height) {
+        fprintf(stderr, "The kernel matrix should not be bigger than the input image size \n");
+        return EXIT_FAILURE;
+    }
+
     uint32_t half_order = kernel_order / 2;
 
     if (kernel_order % 2 == 0) kernel_order += 1; //make it odd to account for edges
@@ -94,6 +94,39 @@ int main(int argc, char * argv[])
         printf("\n");
     }
 
-    return 0;
+    /**Perform the convolution of the kernel matrix onto the input image*/
+    float output_matrix[height][width];
 
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            float pixel_value = 0.0;
+
+            for (int ky = -half_order; ky <= half_order; ky++) {
+                for (int kx = -half_order; kx <= half_order; kx++) {
+                    int ix = x + kx;
+                    int iy = y + ky;
+                    if (ix >= 0 && ix < width && iy >= 0 && iy < height) {
+                        pixel_value += input_matrix[iy][ix] * kernel_matrix[ky + half_order][kx + half_order];
+                    }
+                }
+            }
+            output_matrix[y][x] = (unsigned char)pixel_value;
+        }
+    }
+
+    /**Write to the PGM file */
+    FILE* out = fopen(output_file, "wb");
+    if (!out) {
+        perror("Cannot open file");
+        exit(1);
+    }
+
+    //write PGM header
+    fprintf(out, "P5\n%ld %ld\n%d\n", width, height, max_value);
+    for (int i = 0; i < height; i++) {
+        fwrite(output_matrix[i], sizeof(unsigned char), width, out);
+    }
+    fclose(out);
+
+    return 0;
 } 
